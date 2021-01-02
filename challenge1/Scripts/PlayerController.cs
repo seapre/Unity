b37d81c79c04042108e5ea9d71060d7fed2b6ac8
seapre <seapre@msn.com>
2021-01-02 12:54:40 -0700

    challenge1

---
+++ challenge1/Scripts/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private float rotateSpeed = -15.0f;
+    private float horizontalInput;
+    private float verticalInput;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = 10.0f;
+        //horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+        // move vehicle forward
+        //transform.Translate(0, 0, 1);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
+        //transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.right * Time.deltaTime * rotateSpeed * verticalInput);
+    }
+}
